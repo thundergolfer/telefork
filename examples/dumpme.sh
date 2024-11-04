7575b46f9ca5593e31cd97c37cee5cdf39081a45
Jonathon Belotti <jonathon@modal.com>
2024-11-04 00:56:06 +0000

    Add commentary

--- examples/dumpme.sh
+++ examples/dumpme.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
+#
+# Trivial bash script that can be dumped with dump.rs and restored
+# with load.rs.
+#
+# It does restore successfully (nice!) but the script crashes on teardown
+# with
+#
+# ./examples/dumpme.sh: error reading input file: Bad file descriptor
+#
+# I think this is because telefork isn't restoring any file descriptors.
 
 for i in {1..20}
 do
